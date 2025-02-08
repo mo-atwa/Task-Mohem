6288685b7e600cae5246d1d94f84da5e43ee5f39
Mohamed Atwa <mohamed.872872d@gmail.com>
2025-02-08 11:55:27 +0200

    v2

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/REPO_NAME/',  // استبدل REPO_NAME باسم المستودع
-  plugins: [react()]
-})
